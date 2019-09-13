845b5a6c7a57ff6a61deafcce2f086dbd851b8ac
Elineka <elinekamaleo@gmail.com>
2019-09-13 16:08:34 +0300

    "feature #6 Addition of more pages and functionality"

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+var workplace = document.querySelector("#workplace");
+hello = () => {
+  workplace.style.backgroundColor = "transparent";
+};
+hey = () => {
+  workplace.style.backgroundColor = "rgb(128, 0, 0)";
+};
+workplace.addEventListener("mouseover", hello);
+workplace.addEventListener("mouseout", hey);
+
+var account = document.querySelector(".create");
+signAccount = () => {
+  account.style.backgroundColor = "transparent";
+};
+account.addEventListener("mouseover", signAccount);
+signAccount2 = () => {
+  account.style.backgroundColor = "rgb(128, 0, 0)";
+};
+account.addEventListener("mouseout", signAccount2);
+
+var loginIn = document.querySelector(".enter");
+loginAccount = () => {
+  loginIn.style.backgroundColor = "transparent";
+};
+loginIn.addEventListener("mouseover", loginAccount);
+loginAccount2 = () => {
+  loginIn.style.backgroundColor = "rgb(128, 0, 0)";
+};
+loginIn.addEventListener("mouseout", loginAccount2);
+
+var name = document.querySelector("profile");
+profilePage = () => {
+  name.textContent = profile;
+};
+name.addEventListener("mouseover", profilePage);
