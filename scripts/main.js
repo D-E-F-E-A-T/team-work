b15f495f191309daf2173e7c8857eb5e3f92c704
unknown <you@example.com>
2019-10-10 12:16:32 +0300

    feat #8 responsive home page

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,36 @@
-var workplace = document.querySelector("#workplace");
-hello = () => {
-  workplace.style.backgroundColor = "transparent";
-};
-hey = () => {
-  workplace.style.backgroundColor = "rgb(128, 0, 0)";
-};
-workplace.addEventListener("mouseover", hello);
-workplace.addEventListener("mouseout", hey);
+// var workplace = document.querySelector("#workplace");
+// hello = () => {
+//   workplace.style.backgroundColor = "transparent";
+// };
+// hey = () => {
+//   workplace.style.backgroundColor = "rgb(128, 0, 0)";
+// };
+// workplace.addEventListener("mouseover", hello);
+// workplace.addEventListener("mouseout", hey);
 
-var account = document.querySelector(".create");
-signAccount = () => {
-  account.style.backgroundColor = "transparent";
-};
-account.addEventListener("mouseover", signAccount);
-signAccount2 = () => {
-  account.style.backgroundColor = "rgb(128, 0, 0)";
-};
-account.addEventListener("mouseout", signAccount2);
+// var account = document.querySelector(".create");
+// signAccount = () => {
+//   account.style.backgroundColor = "transparent";
+// };
+// account.addEventListener("mouseover", signAccount);
+// signAccount2 = () => {
+//   account.style.backgroundColor = "rgb(128, 0, 0)";
+// };
+// account.addEventListener("mouseout", signAccount2);
 
-var loginIn = document.querySelector(".enter");
-loginAccount = () => {
-  loginIn.style.backgroundColor = "transparent";
-};
-loginIn.addEventListener("mouseover", loginAccount);
-loginAccount2 = () => {
-  loginIn.style.backgroundColor = "rgb(128, 0, 0)";
-};
-loginIn.addEventListener("mouseout", loginAccount2);
+// var loginIn = document.querySelector(".enter");
+// loginAccount = () => {
+//   loginIn.style.backgroundColor = "transparent";
+// };
+// loginIn.addEventListener("mouseover", loginAccount);
+// loginAccount2 = () => {
+//   loginIn.style.backgroundColor = "rgb(128, 0, 0)";
+// };
+// loginIn.addEventListener("mouseout", loginAccount2);
 
-var name = document.querySelector(".workplaceInput");
-groupCh = () => {
- alert("hoho");
+// var name = document.querySelector(".workplaceInput");
+// groupCh = () => {
+//  alert("hoho");
   
-};
-name.addEventListener("click", groupCh);
+// };
+// name.addEventListener("click", groupCh);
